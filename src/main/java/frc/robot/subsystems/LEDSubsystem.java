68bbbb4eec4ff491727c42f3aece4464c02d3331
chimpion <redraccoon28@gmail.com>
2024-02-04 20:23:02 -0800

    Attempted to add gradients

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -6,6 +6,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.LEDConstants;
+
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.Timer;
@@ -88,18 +89,40 @@ public class LEDSubsystem extends SubsystemBase {
     SOLID_RED (Double.POSITIVE_INFINITY, new Color(255, 0, 0)),
     
     FLASHING_GREEN (0.2, new Color(0,255, 0), Color.off()),
-    FLASHING_BLUE (0.2, new Color(0, 0, 255), Color.off())
-    ;
+    FLASHING_BLUE (0.2, new Color(0, 0, 255), Color.off()),
+    
+    RED_BLUE_GRADIENT (Double.POSITIVE_INFINITY, LightBlendMode.GRADIENT, new Color(255, 0, 0), new Color(0, 0, 255)),
+    RED_BLUE_SCROLL (Double.POSITIVE_INFINITY, LightBlendMode.SCROLL, new Color(255, 0, 0), new Color(0, 0, 255));
 
     Color[] colors;
     double interval;
     int currentColorIndex;
-    
+    LightBlendMode blendMode;
+    /**
+     * @param interval
+     * @param blendMode
+     * @param colors
+     */
+    private LightState(double interval, LightBlendMode blendMode, Color... colors)
+    {
+      this.blendMode = blendMode;
+      switch (this.blendMode){
+        case SCROLL:
+        case GRADIENT:
+          this.colors = Color.createGradient(150, colors);
+          this.interval = Double.POSITIVE_INFINITY;
+          this.currentColorIndex = 0;
+          break;
+        default:
+          this.colors = colors;
+          this.interval = interval;
+          this.currentColorIndex = 0;
+          break;
+      }
+    }
     private LightState(double interval, Color... colors)
     {
-      this.colors = colors;
-      this.interval = interval;
-      this.currentColorIndex = 0;
+      this(interval, LightBlendMode.SOLID, colors);
     }
     /**
      * Gets the current color from array
@@ -107,7 +130,15 @@ public class LEDSubsystem extends SubsystemBase {
      */
     public Color getColor()
     {
-      return this.colors[this.currentColorIndex];
+      if(this.blendMode == LightBlendMode.SOLID)
+      {
+        return this.colors[this.currentColorIndex];
+      }
+      else
+      {
+        cycleColors();
+        return this.colors[this.currentColorIndex];
+      }
     }
 
     /**
@@ -121,6 +152,10 @@ public class LEDSubsystem extends SubsystemBase {
       }
       this.currentColorIndex++;
      }
+  }
+  private enum LightBlendMode{
+    SOLID,
+    GRADIENT, SCROLL
   }
 
   private static class Color{
@@ -170,6 +205,59 @@ public class LEDSubsystem extends SubsystemBase {
      */
     public int getBlue(){
       return this.blue;
+    }
+    /**Linear interpolation between two colors 
+     * (http://www.java2s.com/example/java-utility-method/color-interpolate/interpolate-color-low-color-high-double-min-double-max-double-v-64b61.html)
+     * @param low Color to associate with the min value
+     * @param high Color to associate with the max value
+     * @param min Smallest value that will be passed
+     * @param max Largest value that will be passed
+     * @param v Current value 
+     * **/
+    public static Color interpolate(Color low, Color high, double min, double max, double v) {
+      if (v > max) {
+          v = max;
+      }
+      if (v < min) {
+          v = min;
+      }
+      double distance = 1 - ((max - v) / (max - min));
+      if (Double.isNaN(distance) || Double.isInfinite(distance)) {
+          return high;
+      }
+      int r = (int) weightedAverage(high.getRed(), low.getRed(), distance);
+      int g = (int) weightedAverage(high.getGreen(), low.getGreen(), distance);
+      int b = (int) weightedAverage(high.getBlue(), low.getBlue(), distance);
+      return new Color(r, g, b);
+  }
+    /**Weighted average between two values
+     * 
+     * @param min The lowest value to expect
+     * @param max The highest value to expect
+     * @param p The desired percentage offset between max and min 
+     * @return The resulting value
+     */
+    public static double weightedAverage(double min, double max, double p) {
+      return (min - max) * p + max;
+    }
+
+    public static Color[] createGradient(int length, Color[] colors)
+    {
+      Color[] gradient = new Color[length];
+      int distanceBetweenColors = length / colors.length;
+      int indexOffset = 0;
+      for(int i = 1; i < colors.length; i++)
+      {
+        int lowIndex = indexOffset;
+        int highIndex = indexOffset + distanceBetweenColors;
+        for(int j=0; j < distanceBetweenColors; j++)
+        {
+          gradient[indexOffset] = interpolate(colors[i-1], colors[i], lowIndex, highIndex, indexOffset);
+          indexOffset++;
+        }
+      }
+
+      return gradient;
     }
   }
 }
