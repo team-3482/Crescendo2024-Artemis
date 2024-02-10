9289e60191fcea79c1b792c02615b8685de86529
chimpion <redraccoon28@gmail.com>
2024-02-09 19:47:37 -0800

    Removed support for gradient and scrolling

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -25,9 +25,9 @@ public class LEDSubsystem extends SubsystemBase {
   }
 
   // LED
-  private AddressableLED led = new AddressableLED(LEDConstants.LED_PORT);
+  private static AddressableLED led;
   // LED Buffer
-  private AddressableLEDBuffer ledBuffer = new AddressableLEDBuffer(LEDConstants.LED_COUNT);
+  private static AddressableLEDBuffer ledBuffer;
 
   private double lastLedUpdate = 0.0;
   private LightState state;
@@ -39,11 +39,11 @@ public class LEDSubsystem extends SubsystemBase {
   {
     instance = this;
 
-    this.led = new AddressableLED(LEDConstants.LED_PORT);
-    this.led.setLength(ledBuffer.getLength());
-    this.led.start();
+    ledBuffer = new AddressableLEDBuffer(LEDConstants.LED_COUNT);
 
-    this.ledBuffer = new AddressableLEDBuffer(LEDConstants.LED_COUNT);
+    led = new AddressableLED(LEDConstants.LED_PORT);
+    led.setLength(ledBuffer.getLength());
+    led.start();
 
     this.lastLedUpdate = Timer.getFPGATimestamp();
     this.state = LightState.OFF;
@@ -65,9 +65,9 @@ public class LEDSubsystem extends SubsystemBase {
     
     // Assign Colors
     for (int i = 0; i < ledBuffer.getLength(); i++) {
-      this.ledBuffer.setRGB(i, color.getRed(), color.getGreen(), color.getBlue());
+      ledBuffer.setRGB(i, color.getRed(), color.getGreen(), color.getBlue());
     }
-    this.led.setData(this.ledBuffer);
+    led.setData(ledBuffer);
   };
 
   /**
@@ -90,39 +90,23 @@ public class LEDSubsystem extends SubsystemBase {
     
     FLASHING_GREEN (0.2, new Color(0,255, 0), Color.off()),
     FLASHING_BLUE (0.2, new Color(0, 0, 255), Color.off()),
+    ;
     
-    RED_BLUE_GRADIENT (Double.POSITIVE_INFINITY, LightBlendMode.GRADIENT, new Color(255, 0, 0), new Color(0, 0, 255)),
-    RED_BLUE_SCROLL (Double.POSITIVE_INFINITY, LightBlendMode.SCROLL, new Color(255, 0, 0), new Color(0, 0, 255));
-
     Color[] colors;
     double interval;
     int currentColorIndex;
-    LightBlendMode blendMode;
+    int currentPixelOffset;
     /**
      * @param interval
      * @param blendMode
      * @param colors
      */
-    private LightState(double interval, LightBlendMode blendMode, Color... colors)
-    {
-      this.blendMode = blendMode;
-      switch (this.blendMode){
-        case SCROLL:
-        case GRADIENT:
-          this.colors = Color.createGradient(150, colors);
-          this.interval = Double.POSITIVE_INFINITY;
-          this.currentColorIndex = 0;
-          break;
-        default:
-          this.colors = colors;
-          this.interval = interval;
-          this.currentColorIndex = 0;
-          break;
-      }
-    }
     private LightState(double interval, Color... colors)
     {
-      this(interval, LightBlendMode.SOLID, colors);
+      this.colors = colors;
+      this.interval = interval;
+      this.currentColorIndex = 0;
+      this.currentPixelOffset = 0;
     }
     /**
      * Gets the current color from array
@@ -130,32 +114,19 @@ public class LEDSubsystem extends SubsystemBase {
      */
     public Color getColor()
     {
-      if(this.blendMode == LightBlendMode.SOLID)
-      {
-        return this.colors[this.currentColorIndex];
-      }
-      else
-      {
-        cycleColors();
-        return this.colors[this.currentColorIndex];
-      }
+      return this.colors[this.currentColorIndex];
     }
-
     /**
      * Updates the current color index to get the next color in the color array
      */
     public void cycleColors()
     {
-      if(this.currentColorIndex >= this.colors.length)
+      if(this.currentColorIndex >= this.colors.length - 1)
       {
         this.currentColorIndex = -1; // set to -1 to ensure doesnt skip first color
       }
       this.currentColorIndex++;
      }
-  }
-  private enum LightBlendMode{
-    SOLID,
-    GRADIENT, SCROLL
   }
 
   private static class Color{
@@ -205,59 +176,6 @@ public class LEDSubsystem extends SubsystemBase {
      */
     public int getBlue(){
       return this.blue;
-    }
-    /**Linear interpolation between two colors 
-     * (http://www.java2s.com/example/java-utility-method/color-interpolate/interpolate-color-low-color-high-double-min-double-max-double-v-64b61.html)
-     * @param low Color to associate with the min value
-     * @param high Color to associate with the max value
-     * @param min Smallest value that will be passed
-     * @param max Largest value that will be passed
-     * @param v Current value 
-     * **/
-    public static Color interpolate(Color low, Color high, double min, double max, double v) {
-      if (v > max) {
-          v = max;
-      }
-      if (v < min) {
-          v = min;
-      }
-      double distance = 1 - ((max - v) / (max - min));
-      if (Double.isNaN(distance) || Double.isInfinite(distance)) {
-          return high;
-      }
-      int r = (int) weightedAverage(high.getRed(), low.getRed(), distance);
-      int g = (int) weightedAverage(high.getGreen(), low.getGreen(), distance);
-      int b = (int) weightedAverage(high.getBlue(), low.getBlue(), distance);
-      return new Color(r, g, b);
-  }
-    /**Weighted average between two values
-     * 
-     * @param min The lowest value to expect
-     * @param max The highest value to expect
-     * @param p The desired percentage offset between max and min 
-     * @return The resulting value
-     */
-    public static double weightedAverage(double min, double max, double p) {
-      return (min - max) * p + max;
-    }
-
-    public static Color[] createGradient(int length, Color[] colors)
-    {
-      Color[] gradient = new Color[length];
-      int distanceBetweenColors = length / colors.length;
-      int indexOffset = 0;
-      for(int i = 1; i < colors.length; i++)
-      {
-        int lowIndex = indexOffset;
-        int highIndex = indexOffset + distanceBetweenColors;
-        for(int j=0; j < distanceBetweenColors; j++)
-        {
-          gradient[indexOffset] = interpolate(colors[i-1], colors[i], lowIndex, highIndex, indexOffset);
-          indexOffset++;
-        }
-      }
-
-      return gradient;
     }
   }
 }
