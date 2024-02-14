40b2615ef0225c5831922f818b7a5613c84a1b3f
Anonyma <anonymadly@gmail.com>
2024-02-14 02:41:26 -0800

    Remove unnecessary imports

--- src/main/java/frc/robot/utilities/Logger.java
+++ src/main/java/frc/robot/utilities/Logger.java
@@ -4,7 +4,6 @@
 
 package frc.robot.utilities;
 
-import com.fasterxml.jackson.databind.ser.std.FileSerializer;
 import com.pathplanner.lib.auto.AutoBuilder;
 
 import edu.wpi.first.math.geometry.Pose2d;
@@ -12,7 +11,6 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructArrayPublisher;
 import edu.wpi.first.networktables.StructPublisher;
-import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.SwerveSubsystem;
 
