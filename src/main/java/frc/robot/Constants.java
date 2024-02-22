ddfe368e744b49dbf0443bc6f2a1a4b61c35cf70
Max Cooley <max@polygonal.place>
2024-02-21 16:29:19 -0800

    use correct can id

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -32,7 +32,7 @@ public final class Constants {
   public static class IntakeConstants {
     public static final int LEFT_MOTOR_ID = 5;
     public static final int RIGHT_MOTOR_ID = 3;
-    public static final int INTAKE_MOTOR_ID = 4;
+    public static final int INTAKE_MOTOR_ID = 6;
 
     public static final double INTAKE_SPEED = 0.25;
   }
