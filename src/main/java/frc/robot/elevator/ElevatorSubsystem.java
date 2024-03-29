92baa600d23768ed2905c92c10524ad1bf6a75d9
Max Cooley <max@polygonal.place>
2024-03-29 10:27:55 -0700

    rename climb dir to elevator for consistency

--- src/main/java/frc/robot/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/elevator/ElevatorSubsystem.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.climb;
+package frc.robot.elevator;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
